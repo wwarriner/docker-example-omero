1e73217ed25e631880c99715ea0e73055c40e842
wwarriner <wwarriner@gmail.com>
2020-08-26 16:37:11 -0500

    reordered startup/shutdown for container avail

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,4 @@
-cd /home/vagrant/docker
-docker-compose stop
 cd /home/vagrant/scripts
 ./backup_db.sh
+cd /home/vagrant/docker
+docker-compose stop
--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
-cd /home/vagrant/scripts
-./restore_db.sh
 cd /home/vagrant/docker
 docker-compose start
+cd /home/vagrant/scripts
+./restore_db.sh
